9285114451998dadec09fc7a9ffb35666fc9e4fc
sreymom.quiz <SREYMOM.CHAK@pnc.lan>
2024-06-11 20:08:51 +0700

    post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,31 +13,37 @@ class PostController extends Controller
     public function index()
     {
         //
+        $posts = Post::list();
+        return response()->json(['success' => true, 'posts' => $posts]);
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
     {
-        //
+        Post::store($request);
+        return response()->json([
+            'success' => true,
+            'data' => true,
+            'message' => 'post created successfully'
+        ], 200);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Post $post)
+    public function show(string $id)
     {
-        //
+
+        $post = Post::find($id);
+        return response()->json(['success' => true, 'data' => $post], 200);
     }
+
 
     /**
      * Show the form for editing the specified resource.
@@ -50,16 +56,32 @@ class PostController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Post $post)
+    public function update(Request $request, $id)
     {
         //
+        Post::store($request, $id);
+        return response()->json([
+            'success' => true,
+            'data' => true,
+            'message' => 'post update successfully'
+        ], 200);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Post $post)
+    public function destroy(string $id)
     {
-        //
+        $post = Post::find($id);
+        $post->delete();
+        return response()->json([
+            'success' => true,
+            'data' => true,
+            'message' => 'post delete successfully'
+        ], 200);
     }
 }
+
+
+
+
